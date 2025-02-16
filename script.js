51f18b2c7fb372679c930885d969d9a69f53ee94
Abhinav Gupta <gabhinav95a@gmail.com>
2025-02-17 00:54:43 +0530

    added delete functionality

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 const addBtn = document.getElementById("addTaskBtn");
+const list = document.querySelector(".list-wrapper");
 
 addBtn.addEventListener("click", function () {
   const inputTask = document.getElementById("taskInput");
   const inputValue = inputTask.value.trim();
 
-  const list = document.querySelector(".list-wrapper");
   if (inputValue) {
+    const id = Date.now();
     const listItem = document.createElement("li");
-    listItem.setAttribute("id", list.childElementCount + 1);
-    listItem.innerHTML = `<input type="checkbox" /><span class="item-name">${inputValue}</span><button id="edit">Edit</button><button id="delete">Delete</button>`;
+    listItem.setAttribute("id", id);
+    listItem.innerHTML = `<span class="item-name">${inputValue}</span><button class="delete-btn">Delete</button>`;
     list.appendChild(listItem);
+  }
+});
+
+list.addEventListener("click", function (e) {
+  if (e.target?.classList.contains("delete-btn")) {
+    const parentElement = e.target.parentElement;
+    parentElement.remove();
   }
 });
