b33ebe6b46378456eb939b5c6b7ed64884b97170
Abhinav Gupta <gabhinav95a@gmail.com>
2025-02-26 23:18:36 +0530

    added edit functionality to list

--- script.js
+++ script.js
@@ -1,16 +1,34 @@
 const addBtn = document.getElementById("addTaskBtn");
 const list = document.querySelector(".list-wrapper");
+const inputTask = document.getElementById("taskInput");
+
+let isEditing = false;
+let currentEditItem = null;
+
+inputTask.addEventListener("input", function () {
+  const inputValue = inputTask.value.trim();
+  if (isEditing && currentEditItem) {
+    addBtn.disabled = !inputValue || inputValue === currentEditItem.textContent;
+  } else addBtn.disabled = !inputTask.value.trim();
+});
 
 addBtn.addEventListener("click", function () {
-  const inputTask = document.getElementById("taskInput");
   const inputValue = inputTask.value.trim();
 
   if (inputValue) {
-    const id = Date.now();
-    const listItem = document.createElement("li");
-    listItem.setAttribute("id", id);
-    listItem.innerHTML = `<span class="item-name">${inputValue}</span><button class="delete-btn">Delete</button>`;
-    list.appendChild(listItem);
+    if (isEditing && currentEditItem) {
+      currentEditItem.textContent = inputValue;
+      addBtn.innerHTML = "Add";
+      isEditing = false;
+      currentEditItem = null;
+    } else {
+      const id = Date.now();
+      const listItem = document.createElement("li");
+      listItem.setAttribute("id", id);
+      listItem.innerHTML = `<input type="checkbox" /><span class="item-name">${inputValue}</span><button class="edit-btn">Edit</button><button class="delete-btn">Delete</button>`;
+      list.appendChild(listItem);
+    }
+    inputTask.value = "";
   }
 });
 
@@ -18,5 +36,13 @@ list.addEventListener("click", function (e) {
   if (e.target?.classList.contains("delete-btn")) {
     const parentElement = e.target.parentElement;
     parentElement.remove();
+  }
+  if (e.target?.classList.contains("edit-btn")) {
+    const parentElement = e.target.parentElement;
+    addBtn.innerHTML = "Save";
+    currentEditItem = parentElement.querySelector(".item-name");
+    inputTask.value = currentEditItem.textContent;
+    isEditing = true;
+    addBtn.disabled = true;
   }
 });
